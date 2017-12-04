46d6184f34e871d69eee4662d89187c1f586adca
Caê Penna <caepenna@gmail.com>
2017-12-04 17:08:47 -0200

    🐛 Fix body scroll when nav is open

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,12 @@ var nav = $('.edem-header');
 
 $(window).on('scroll', function () {
   var currentPosition = $(this).scrollTop();
-  
   sections.each(function() {
     var top = $(this).offset().top - nav.outerHeight() - 32,
         bottom = top + $(this).outerHeight();
-    
     if (currentPosition >= top && currentPosition <= bottom) {
       nav.find('a').removeClass('-active');
       sections.removeClass('-active');
-      
       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('-active');
     }
   });
@@ -20,41 +17,27 @@ $(window).on('scroll', function () {
 $('.js-anchor').on('click', function () {
   var $el = $(this)
     , id = $el.attr('href');
-  
   $('html, body').animate({
     scrollTop: $(id).offset().top - nav.outerHeight() + 1
   }, 500);
-  
   return false;
 });
 
 $(document).ready(function(){
+
+  // Navigation menu
   $('.toggle-menu').on('click', function(){
     $(this).toggleClass('-x');
     $('.navigation-menu').toggleClass('-open');
+    $('body').toggleClass('-noscroll');
   });
-  $('.navigation-menu > a').on('click', function(){
+  $('.navigation-menu > a, .edem-logo').on('click', function(){
     $('.toggle-menu').removeClass('-x');
     $('.navigation-menu').removeClass('-open');
+    $('body').removeClass('-noscroll');
   });
-  $('.edem-logo').on('click', function(){
-    $('.toggle-menu').removeClass('-x');
-    $('.navigation-menu').removeClass('-open');
-  });
-});
 
-$(document).on('scroll', function() {
-
-  if($(document).scrollTop()>96) {
-    $('.edem-header').addClass('-small');
-  } else {
-    $('header').removeClass('-small');
-  }
-  
-});
-
-$(document).ready(function() {
-
+  // Text input
   $('.js-input').blur(function() {
     if ($(this).val()) {
       $(this).closest( '.input-container' ).addClass('-filled');
@@ -63,4 +46,13 @@ $(document).ready(function() {
     }
   });
 
+});
+
+// Header height
+$(document).on('scroll', function() {
+  if($(document).scrollTop()>96) {
+    $('.edem-header').addClass('-small');
+  } else {
+    $('header').removeClass('-small');
+  }  
 });
